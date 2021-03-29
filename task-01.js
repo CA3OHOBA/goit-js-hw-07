e199828a33d1fe7a3492ec2c68e4432b11b60385
CA3OHOBA <sofiiazhah@gmailcom>
2021-03-29 14:06:20 +0300

    +

--- task-01.js
+++ task-01.js
@@ -7,9 +7,6 @@ console.log(`В списке ${li_items.length} категории.`);
 // Для каждого элемента li.item в списке ul#categories, найдет и выведет в консоль 
 // текст заголовка элемента (тега h2) и количество элементов в категории (всех вложенных в него элементов li).
 
-// Категория: 
-// Количество элементов: 
-
 li_items.forEach(el => {
     console.log(`Категория: ${el.querySelector('h2').textContent}`);
     console.log(`Количество элементов: ${el.querySelectorAll('li').length}`);
