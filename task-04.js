8ff3fceac4e6202add91b584fec8d0cf2b70fb93
CA3OHOBA <sofiiazhah@gmailcom>
2021-03-29 21:15:31 +0300

    task-04

--- task-04.js
+++ task-04.js
@@ -3,3 +3,26 @@
 // Создай переменную counterValue в которой будет хранится текущее значение счетчика.
 // Создай функции increment и decrement для увеличения и уменьшения значения счетчика
 // Добавь слушатели кликов на кнопки, вызовы функций и обновление интерфейса
+
+
+const counterValue = document.querySelector('#value');
+const incrementBtn = document.querySelector('button[data-action="increment"]');
+const decrementBtn = document.querySelector('button[data-action="decrement"]');
+
+let value = 0;
+
+
+const increment = () => {
+    value++;
+    counterValue.textContent = value;
+}
+
+
+const decrement = () => {
+    value--;
+    counterValue.textContent = value;
+}
+    
+    
+incrementBtn.addEventListener('click', increment);
+decrementBtn.addEventListener('click', decrement);
