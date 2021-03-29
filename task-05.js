c2ee82dab115a166d537bbeb9d848a27b2fb5f31
CA3OHOBA <sofiiazhah@gmailcom>
2021-03-29 23:08:06 +0300

    task-05

--- task-05.js
+++ task-05.js
@@ -1,1 +1,20 @@
-// Напиши скрипт который, при наборе текста в инпуте input#name-input (событие input), подставляет его текущее значение в span#name-output. Если инпут пустой, в спане должна отображаться строка 'незнакомец'.
+// Напиши скрипт который, при наборе текста в инпуте input#name-input 
+// (событие input), подставляет его текущее значение в span#name-output.
+//  Если инпут пустой, в спане должна отображаться строка 'незнакомец'.
+
+
+const input = document.querySelector('input');
+const output = document.querySelector('#name-output');
+
+
+const newInput = (word) => {
+  const result = word.target.value.length > 0 ?
+   output.textContent = word.target.value :
+   output.textContent = 'незнакомец';
+  
+}
+
+
+
+input.addEventListener('input', newInput);
+
